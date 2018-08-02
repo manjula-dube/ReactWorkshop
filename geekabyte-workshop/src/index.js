1352b1c81d298b4b2535577152a073284e693391
Manjula Dube <dube.manjula668@gmail.com>
2018-08-02 18:47:55 +0200

    working router

--- geekabyte-workshop/src/index.js
+++ geekabyte-workshop/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import AppRouter from "./router";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppRouter/>, document.getElementById('root'));
 registerServiceWorker();
--- geekabyte-workshop/src/router.js
+++ geekabyte-workshop/src/router.js
@@ -1,14 +1,14 @@
 import React from "react"
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import App from "./App"
 import About from "./About"
 
  const AppRouter = () => (
 	<Router>
 		<div>
-			<Route exact path="/" component={App} />
-			<Route exact path="/about" component={About} />
-		</div>
+				<Route exact path="/" component={App} />
+				<Route path="/about" component={About} />
+		</div>		
 	</Router>
 )
  export default AppRouter
